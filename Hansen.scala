5c9a0efee5f64938fa73b69c64a0bb789c61c083
AliceCengal <cengallut@gmail.com>
2014-10-12 00:04:49 -0500

    modules, formatting

--- Hansen.scala
+++ Hansen.scala
@@ -38,27 +38,27 @@ object Hansen {
         .*(1/sumProp)
     }
     
-    val aceticAcid = Solvent(6.8, 6.0, 9.2)
+    val ACETIC_ACID = Solvent(6.8, 6.0, 9.2)
     
-    val dioctylPhtalate = Solvent(8.1, 3.4, 1.5)
+    val DIOCTYL_PHTALATE = Solvent(8.1, 3.4, 1.5)
     
-    val nitroamine = Solvent(8.6, 6.8, 0.0)
+    val NITROAMINE = Solvent(8.6, 6.8, 0.0)
     
     /* Mystery solvent MEP */
-    val mep = Solvent(7.8, 4.4, 2.5)
+    val MEP = Solvent(7.8, 4.4, 2.5)
     
   }
   
   def main(args: Array[String]) {
     val prospective =
       Solvent.combine(List(
-        (Solvent.aceticAcid,      0.2),
-        (Solvent.dioctylPhtalate, 0.5),
-        (Solvent.nitroamine,      0.3)
+        (Solvent.ACETIC_ACID,      0.2),
+        (Solvent.DIOCTYL_PHTALATE, 0.5),
+        (Solvent.NITROAMINE,      0.3)
       ))
     
     print("R-value for 20% AA, 50% DP, 30% NA:")
-    println(prospective.r_value(Solvent.mep))
+    println(prospective.r_value(Solvent.MEP))
     
     val props = 0.0 to 1.0 by 0.01
     var bestProportion = (0.0,0.0,0.0)
@@ -71,11 +71,11 @@ object Hansen {
         na  = 1.0 - aa - dp) {
       val prospective = 
         Solvent.combine(List(
-          (Solvent.aceticAcid,      aa),
-          (Solvent.dioctylPhtalate, dp),
-          (Solvent.nitroamine,      na)))
+          (Solvent.ACETIC_ACID,      aa),
+          (Solvent.DIOCTYL_PHTALATE, dp),
+          (Solvent.NITROAMINE,       na)))
       
-      val currentR = prospective.r_value(Solvent.mep)
+      val currentR = prospective.r_value(Solvent.MEP)
       
       if (currentR < bestRvalue) {
         bestRvalue = currentR
--- src/hansen.rs
+++ src/hansen.rs
@@ -1,6 +1,8 @@
 
+mod solvent {
+
 /* A solvent with the three Hansen parameters. */
-struct Solvent {
+pub struct Solvent {
     d_param: f64,
     p_param: f64,
     h_param: f64
@@ -22,7 +24,7 @@ impl Solvent {
             h_param: self.h_param + other.h_param}
     }
     
-    fn r_value(&self, other: Solvent) -> f64 {
+    pub fn r_value(&self, other: Solvent) -> f64 {
         let diffs = vec![
             self.d_param - other.d_param,
             self.p_param - other.p_param,
@@ -32,56 +34,53 @@ impl Solvent {
             .fold(0.0f64, |sum, param| sum + param)
     }
     
-    fn combine(proportionated: &[(Solvent, f64)]) -> Solvent {
-        let sum_proportion =
-            proportionated.iter()
-                .map(|&sp| { let (_,p) = sp; p })
-                .fold(0.0f64, |sum, p| sum + p );
-        
+}
+
+/* Because monoid. */
+pub static NULL_SOLVENT: Solvent =
+    Solvent { d_param: 0.0, p_param: 0.0, h_param: 0.0 };
+
+pub static ACETIC_ACID: Solvent =
+    Solvent { d_param: 6.8, p_param: 6.0, h_param: 9.2 };
+
+pub static DIOCTYL_PHTALATE: Solvent =
+    Solvent { d_param: 8.1, p_param: 3.4, h_param: 1.5 };
+
+pub static NITROAMINE: Solvent =
+    Solvent { d_param: 8.6, p_param: 6.8, h_param: 0.0 };
+
+/* Mystery solvent MEP */
+pub static MEP: Solvent =
+    Solvent { d_param: 7.8, p_param: 4.4, h_param: 2.5 };
+
+pub fn combine(proportionated: &[(Solvent, f64)]) -> Solvent {
+    let sum_proportion =
         proportionated.iter()
-            .map( |&sp| { let (s,p) = sp; s.scale(p) } )
-            .fold(Solvent::null_solvent(), |s1,s2| s1.add(s2))
-            .scale(1.0f64/sum_proportion)
-    }
+            .map(|&sp| { let (_,p) = sp; p })
+            .fold(0.0f64, |sum, p| sum + p );
     
-    /* Because monoid. */
-    fn null_solvent() -> Solvent {
-        Solvent { d_param: 0.0, p_param: 0.0, h_param: 0.0 }
-    }
-    
-    fn acetic_acid() -> Solvent {
-        Solvent { d_param: 6.8, p_param: 6.0, h_param: 9.2 }
-    }
-    
-    fn dioctyl_phtalate() -> Solvent {
-        Solvent { d_param: 8.1, p_param: 3.4, h_param: 1.5 }
-    }
-    
-    fn nitroamine() -> Solvent {
-        Solvent { d_param: 8.6, p_param: 6.8, h_param: 0.0 }
-    }
-    
-    /* Mystery solvent MEP */
-    fn mep() -> Solvent {
-        Solvent { d_param: 7.8, p_param: 4.4, h_param: 2.5 }
-    }
-    
+    proportionated.iter()
+        .map( |&sp| { let (s,p) = sp; s.scale(p) } )
+        .fold(NULL_SOLVENT, |s1,s2| s1.add(s2))
+        .scale(1.0f64/sum_proportion)
+}
+
 }
 
 fn main() {
     let prospective =
-        Solvent::combine([
-            (Solvent::acetic_acid(),      2.0),
-            (Solvent::dioctyl_phtalate(), 5.0),
-            (Solvent::nitroamine(),       3.0)
+        solvent::combine([
+            (solvent::ACETIC_ACID,      2.0),
+            (solvent::DIOCTYL_PHTALATE, 5.0),
+            (solvent::NITROAMINE,       3.0)
         ]);
     
     println!("R-value for 20% AA, 50% DP 30% NA: {}", 
-            prospective.r_value(Solvent::mep()));
+            prospective.r_value(solvent::MEP));
     
     let mut best_proportion = (0f64, 0f64, 0f64);
     let mut best_r_value = std::f64::MAX_VALUE;
-    let target_solvent = Solvent::mep();
+    let target_solvent = solvent::MEP;
     
     let precision = 100i;
     
@@ -90,10 +89,10 @@ fn main() {
             if aa + dp <= precision {
                 let na = precision - aa - dp;
                 let prospective =
-                    Solvent::combine([
-                        (Solvent::acetic_acid(),      aa as f64),
-                        (Solvent::dioctyl_phtalate(), dp as f64),
-                        (Solvent::nitroamine(),       na as f64)
+                    solvent::combine([
+                        (solvent::ACETIC_ACID,      aa as f64),
+                        (solvent::DIOCTYL_PHTALATE, dp as f64),
+                        (solvent::NITROAMINE,       na as f64)
                     ]);
                 
                 let current_r = prospective.r_value(target_solvent);
